55cc369112c536bbb987592141d6ef6379518c85
Haitham Alrubai <72894675+manutd9393@users.noreply.github.com>
2025-03-20 14:03:35 -0500

    update

--- vnet/main.tf
+++ vnet/main.tf
@@ -7,27 +7,21 @@ terraform {
   }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_config.name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.vnet_config.location
+  resource_group_name = var.vnet_config.resource_group_name
   address_space       = var.vnet_config.address_space
 }
 
 resource "azurerm_subnet" "subnets" {
   for_each            = var.vnet_config.subnets
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = var.vnet_config.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [each.value.prefix]
 
-  # ✅ Corrected Private Endpoint Policies
-  private_endpoint_network_policies     = each.value.enable_private_endpoint ? "Disabled" : null
+  private_endpoint_network_policies = each.value.enable_private_endpoint ? "Disabled" : null
 
   dynamic "delegation" {
     for_each = each.value.delegation != null ? [1] : []
@@ -42,4 +36,17 @@ resource "azurerm_subnet" "subnets" {
       }
     }
   }
+}
+
+# ✅ Optional VNet Peering (if peer_with_vnets is provided)
+resource "azurerm_virtual_network_peering" "vnet_peering" {
+  for_each                     = var.peer_with_vnets
+  name                          = "${azurerm_virtual_network.vnet.name}-to-${each.value.name}"
+  resource_group_name           = var.vnet_config.resource_group_name
+  virtual_network_name          = azurerm_virtual_network.vnet.name
+  remote_virtual_network_id     = each.value.id
+  allow_virtual_network_access  = true
+  allow_forwarded_traffic       = true
+  allow_gateway_transit         = false
+  use_remote_gateways           = false
 }
--- vnet/variables.tf
+++ vnet/variables.tf
@@ -1,23 +1,24 @@
-variable "resource_group_name" {
-  description = "The name of the Resource Group"
-  type        = string
-}
-
-variable "location" {
-  description = "Azure region"
-  type        = string
-}
-
 variable "vnet_config" {
   description = "Configuration for the Virtual Network"
   type = object({
-    name          = string
-    address_space = list(string)
-    subnets       = map(object({
+    name                = string
+    address_space       = list(string)
+    resource_group_name = string
+    location            = string
+    subnets             = map(object({
       name                     = string
       prefix                   = string
       enable_private_endpoint  = optional(bool, false)
       delegation               = optional(string)
     }))
   })
+}
+
+variable "peer_with_vnets" {
+  description = "A map of VNets to peer with"
+  type = map(object({
+    name = string
+    id   = string
+  }))
+  default = {}  # ✅ Default is empty (no peering for now)
 }
